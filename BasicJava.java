5820c8a565967c897a2d42946422ed24c9b2feb3
dwisilvira <dwisilvira16@gmail.com>
2021-10-12 12:35:49 +0700

    challenge 1

---
+++ BasicJava.java
@@ -0,0 +1,25 @@
+public class BasicJava {
+    String username;
+    final String lastName = "";
+
+    public static void main(String[] args){
+        String firstName = "Dwi ";
+        String lastName = "Silvira";
+        int age = 20;
+        String address = "Medan,Sumatera Utara";
+        String hobby = "Membaca";
+        String makananKesukaan = "Bakso, Mie goreng";
+        String phoneNumber = "088236588120";
+        
+        System.out.println("first name :" + firstName);
+        System.out.println("last name: " + lastName);
+        System.out.println("full name: " + firstName +lastName);
+        System.out.println("age :" + age);
+        System.out.println("address :" + address);
+        System.out.println("hobby :" + hobby);
+        System.out.println( "makanan kesukaan :" + makananKesukaan);
+        System.out.println("phone Number :" + phoneNumber);
+
+    }
+    
+}
